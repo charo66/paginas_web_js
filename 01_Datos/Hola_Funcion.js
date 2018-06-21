9d1dfda0040f4ed12832be7685f2f3fe1d89b318
charo66 <chagarmol@gmail.com>
2018-06-21 13:58:53 +0200

    funciones en java

---
+++ 01_Datos/Hola_Funcion.js
@@ -0,0 +1,16 @@
+/* 
+JavaScript(@Oracle es una marca registrada) ->ECMAscript ->ES6 / ES2015
+*/
+
+function saludar(user){
+    const saludo = 'Hola'
+    console.log(saludo,user)
+}
+function sumar(a, b)
+{
+    let resultado= a + b
+    console.log(resultado)
+}
+saludar('Pepe')
+saludar('Elena')
+sumar(2,4)
---
+++ 01_Datos/Hola_Mundo.js
@@ -0,0 +1,17 @@
+var saludo
+/* inicialización */
+saludo= 'Hola'
+
+/* 
+Tipos de datos
+-string (cadena de caracteres)
+-number
+-booleans (true/false) 
+-object (todo lo que no sea los anteriores)
+-undefined
+ */
+/* Declaración +inicialización */
+var user = 'Mundo'
+
+console.log (saludo, user)
+
