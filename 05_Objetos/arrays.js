b0f784ce055305be1a849ce15670098130a8746c
charo66 <chagarmol@gmail.com>
2018-07-11 13:48:43 +0200

    Javascript

---
+++ 05_Objetos/arrays.js
@@ -0,0 +1,7 @@
+let oDatos ={}
+//oDatos = new Object()
+
+let aDatos = [1,2]
+// aDatos = new Array()
+
+aDatos.
---
+++ 05_Objetos/elementales.ts
@@ -0,0 +1,22 @@
+function tipos () {
+    let num = 23
+    let nombre = 'Pepe'
+    let isOk = true
+    let aDatos = []
+    let oDatos = {}
+
+    aDatos.push(12)
+    num.toFixed(2)//Number
+    nombre.toUpperCase() //String
+    isOk.valueOf() // Boolean
+}
+
+
+function numbers () {
+    let num =23
+    console.log(num.toFixed(2))
+    num.toFixed(2)
+}
+    function numbersTS() {
+        let num=
+    }
