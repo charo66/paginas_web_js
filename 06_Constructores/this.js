83142fff006c1cf016ad11f7b4e82114fbc1abcb
charo66 <chagarmol@gmail.com>
2018-07-18 13:46:08 +0200

    javascript

---
+++ 06_Constructores/this.js
@@ -0,0 +1,32 @@
+// THIS -> objeto global
+
+/* console.log(this)
+
+function prueba() {
+    console.log(this)
+}
+
+prueba() */
+
+// THIS -> objeto
+
+/* let o = {
+    nonbre: 'Pepe',
+    mostrar: function () {
+        console.log(this)
+    }
+}
+
+o.mostrar() */
+
+// THIS -> el objeto creado
+
+// THIS -> el objeto creado
+
+function cosaRara() {
+    console.log(this)
+}
+
+cosaRara()
+
+//new cosaRara()
